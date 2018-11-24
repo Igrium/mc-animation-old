ab7c164cae450114ed98bff5db82612588737d87
Sam54123 <thesam54123@gmail.com>
2018-11-23 18:54:53 -0800

    started work on java program

---
+++ program/src/com/github/sam54123/mc_animation/system/Animation.java
@@ -0,0 +1,12 @@
+package com.github.sam54123.mc_animation.system;
+
+import java.nio.file.*;
+
+public class Animation 
+{
+	// Import an mcanim (json) file
+	public Animation(String file)
+	{
+		
+	}
+}
