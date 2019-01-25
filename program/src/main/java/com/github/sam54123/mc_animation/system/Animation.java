a1a167e80fed806866cb7f0c743edd63609704e4
Sam54123 <thesam54123@gmail.com>
2019-01-24 21:57:36 -0800

    added documentation to animation class

--- program/src/main/java/com/github/sam54123/mc_animation/system/Animation.java
+++ program/src/main/java/com/github/sam54123/mc_animation/system/Animation.java
@@ -3,7 +3,6 @@ package com.github.sam54123.mc_animation.system;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import org.json.*;
@@ -13,6 +12,9 @@ import com.github.sam54123.mc_animation.utils.MCAnimStatics;
 import com.github.sam54123.mc_animation.utils.MCAnimValidator;
 import com.github.sam54123.mc_animation.utils.ProgramConstants;
 
+/**
+ * Represents a single animation that can be played on an armor stand
+ */
 public class Animation 
 {
 	
@@ -25,17 +27,20 @@ public class Animation
 	private int id;
 	private AnimFrame[] frames;
 	
+	/**
+	 * Has this animation been saved to disk since it's last modification
+	 */
 	public boolean isSaved = true;
-	
 	
 	private boolean isInitialized = false;
 	
 	
-	// Import an mcanim (json) file
+	/**
+	 * Import an mcanim (json) file
+	 * @param path Path to import from
+	 */
 	public Animation(String path)
 	{
-		
-		
 		System.out.println("Opening "+path);
 		
 		// Validate path
@@ -94,7 +99,12 @@ public class Animation
 		isInitialized = true;
 	}
 	
-	// Saves the animation as a .mcanim file and updates path accordingly
+	/**
+	 * Saves the animation as a .mcanim file and updates path accordingly
+	 * @param path Path to save to
+	 * @return Was save successful
+	 * @throws IOException
+	 */
 	public File save(String path) throws IOException
 	{
 		outputToJSON(jsonObject);
@@ -112,12 +122,16 @@ public class Animation
 		
 	}
 	
+	
 	public File save() throws IOException
 	{
 		return save(path);
 	}
 	
-	// Returns the folder this animatin is in, excluding the file itself
+	/**
+	 * Returns the folder this animatin is in, excluding the file itself
+	 * @return Folder
+	 */
 	public String getFolder()
 	{
 		String path = MCAnimStatics.formatPath(this.path);
@@ -128,11 +142,19 @@ public class Animation
 		return path.substring(0, index);
 	}
 	
+	/**
+	 * Returns an array of all this animation's frames
+	 * @return Frames
+	 */
 	public AnimFrame[] getFramesAsArray() 
 	{
 		return frames;
 	}
 	
+	/**
+	 * Returns an array of all this animation's commands
+	 * @return Commands
+	 */
 	public AnimCommand[] getCommandsAsArray()
 	{
 		AnimCommand[] array = new AnimCommand[commands.size()];
@@ -144,21 +166,38 @@ public class Animation
 		return array;
 	}
 	
+	/**
+	 * How long this animation is in frames
+	 * @return Length
+	 */
 	public int length()
 	{
 		return frames.length;
 	}
 	
-	public AnimFrame getFrame(int index)
+	/**
+	 * Returns the Frame object at the given index of the animation
+	 * @param index
+	 * @return Frame
+	 * @throws ArrayIndexOutOfBoundsException
+	 */
+	public AnimFrame getFrame(int index) throws ArrayIndexOutOfBoundsException
 	{
 		return frames[index];
 	}
 	
+	/**
+	 * Returns this animation's animation ID
+	 * @return ID
+	 */
 	public int id()
 	{
 		return id;
 	}
 	
+	/**
+	 * Sets this animation's animation ID
+	 */
 	public void setId(int id)
 	{
 		if (id > 0)
@@ -171,7 +210,11 @@ public class Animation
 		}
 	}
 	
-	// finds the command of the given frame
+	/**
+	 * Gets the command at the given frame
+	 * @param frame
+	 * @return
+	 */
 	public AnimCommand getCommandByFrame(int frame) 
 	{
 
@@ -186,6 +229,11 @@ public class Animation
 		return null;
 	}
 	
+	/**
+	 * Sets a command at the given frame
+	 * @param frame
+	 * @param command Command to set
+	 */
 	public void setCommand(int frame, String command)
 	{
 		// remove current command in frame
@@ -200,6 +248,10 @@ public class Animation
 		commands.add(new AnimCommand(command, frame));
 	}
 	
+	/**
+	 * Gets the function name this animation will take when compiled, excluding file extention
+	 * @return Compiled animation name
+	 */
 	public String getCompiledAnimName()
 	{
 		return "anim-"+id;
@@ -209,6 +261,7 @@ public class Animation
 	{
 		return isInitialized;
 	}
+
 	// interpret the JSONObject and setup all this Animation Object's variables, will crash if invalid JSON.
 	private void interpretJSON(JSONObject jsonObject)
 	{
@@ -275,6 +328,10 @@ public class Animation
 		}
 	}
 	
+	/**
+	 * Outputs this animation to a JSONObject (used when saving to .mcanim)
+	 * @param jsonObject
+	 */
 	public void outputToJSON(JSONObject jsonObject)
 	{
 		// Set basic metadata
